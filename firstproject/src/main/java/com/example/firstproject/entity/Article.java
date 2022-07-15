0a93119e9313351f2d0957cb1f8a5764eac262a0
Chaeyoung <oco6029@naver.com>
2022-07-15 17:55:27 +0900

    0715 JPA 데이터 생성, 롬복&리팩토링

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity // DB 가 해당 객체를 인식 가능
+@AllArgsConstructor
+@ToString
+public class Article {
+
+    @Id //대표값을 지정, 주민등록번호 같은 것
+    @GeneratedValue // 1,2,3.. 자동 생성 annotation
+    private Long id;
+
+    @Column // DB에서 관리하는 table과 연결해주는 단위위
+    private String title;
+
+    @Column
+    private String content;
+}
