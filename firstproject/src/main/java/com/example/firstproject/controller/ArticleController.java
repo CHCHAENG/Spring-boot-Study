38999f242db665fc39d79d6d16b10500a4e9c329
Chaeyoung <oco6029@naver.com>
2022-07-15 18:28:29 +0900

    0715 데이터 목록 조회

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j // 로깅을 위한 annotation
@@ -37,5 +42,33 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 data를 가져옴
+        // id값을 통해서 찾은것, 해당 id 값이 없다면 null을 반환하는 것.
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지를 설정
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+
+        // 1. 모든 Article을 가져옴
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        // 2. 가져온 Article 묶음을 view로 전달
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. view 페이지 설정
+        return "articles/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity // DB 가 해당 객체를 인식 가능
 @AllArgsConstructor
+@NoArgsConstructor  // 디폴트 생성자 추가 annotation
 @ToString
 public class Article {
 
@@ -22,4 +24,5 @@ public class Article {
 
     @Column
     private String content;
+
 }
